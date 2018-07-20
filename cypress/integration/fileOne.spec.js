4b49dda2e8c74c389a349c6ebd9feddb99bc3370
Christian Danielsen <christian.danielsen@insitu.com>
2018-07-20 10:08:54 -0700

    reproducible test case

---
+++ cypress/integration/fileOne.spec.js
@@ -0,0 +1,5 @@
+describe('First test', () => {
+  it('Gets run: cool', () => {
+    expect(true).to.eql(true);
+  })
+})
---
+++ cypress/integration/fileTwo.spec.js
@@ -0,0 +1,5 @@
+describe('Second test', () => {
+  it('Never gets run: frowny face', () => {
+    expect(true).to.eql(true)
+  })
+})
