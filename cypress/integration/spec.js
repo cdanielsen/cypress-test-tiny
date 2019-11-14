0b040c1887a5c06620c6d2003098b04e6dbf5234
Christian Danielsen <christian.danielsen@insitu.com>
2019-11-14 13:08:08 -0800

    setup simple demo test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,9 @@
-describe('page', () => {
-  it('works', () => {})
+describe('setting environment variables via the CYPRESS_ method', () => {
+  it('does not work for certain words', () => {
+    // Arbitrary value works fine
+    expect(Cypress.env('SOME_VAR')).to.eq('someValue');
+
+    // CONFIG_FILE appears to be reserved/does not take
+    expect(Cypress.env('CONFIG_FILE')).to.eq('qa');
+  })
 })
